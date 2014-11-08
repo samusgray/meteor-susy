fe0e66595f2bc3f865225559dd1ec8458e7faeeb
Aaron Gray <aaron@hellometa.com>
2014-11-08 14:58:48 -0700

    Include Susy source

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'meta:susy',
+  summary: 'Powerful grid tools for the web.',
+  version: '1.0.0',
+  git: 'https://github.com/aaronagray/meteor-susy'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use('fourseven:scss@0.9.5');
+  api.addFiles([
+    "_susyone.scss"
+    ], 'server', {
+    isAsset: true
+  });
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('meta:susy');
+  api.addFiles('meta:susy-tests.js');
+});
