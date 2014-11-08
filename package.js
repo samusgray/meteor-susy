ec5a58a03a386adf803fc587a25830e62e83e79a
Aaron Gray <aaron@hellometa.com>
2014-11-08 15:36:20 -0700

    Include all paths

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meta:susy',
   summary: 'Powerful grid tools for the web.',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/aaronagray/meteor-susy'
 });
 
@@ -9,7 +9,58 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('fourseven:scss@1.0.0');
   api.addFiles([
-    "_susyone.scss"
+
+    // Susy API
+    "_susy.scss",
+    "susy/language/_susy.scss",
+    "susy/language/susy/_background.scss",
+    "susy/language/susy/_bleed.scss",
+    "susy/language/susy/_box-sizing.scss",
+    "susy/language/susy/_breakpoint-plugin.scss",
+    "susy/language/susy/_container.scss",
+    "susy/language/susy/_context.scss",
+    "susy/language/susy/_gallery.scss",
+    "susy/language/susy/_grids.scss",
+    "susy/language/susy/_gutters.scss",
+    "susy/language/susy/_isolate.scss",
+    "susy/language/susy/_margin.scss",
+    "susy/language/susy/_padding.scss",
+    "susy/language/susy/_rows.scss",
+    "susy/language/susy/_settings.scss"
+    "susy/language/susy/_span.scss",
+    "susy/language/susy/_validation.scss",
+
+    // Susy Math
+    "susy/_math.scss",
+    "susy/math/_su.scss",
+    "susy/math/su/_grid.scss",
+    "susy/math/su/_maps.scss",
+    "susy/math/su/_settings.scss",
+    "susy/math/su/_validation.scss",
+
+    // Susy Output
+    // - Float
+    "susy/output/_float.scss",
+    "susy/output/float/_container.scss",
+    "susy/output/float/_end.scss",
+    "susy/output/float/_isolate.scss",
+    "susy/output/float/_span.scss",
+    // - Shared
+    "susy/output/_shared.scss",
+    "susy/output/shared/_background.scss",
+    "susy/output/shared/_container.scss",
+    "susy/output/shared/_direction.scss",
+    "susy/output/shared/_inspect.scss",
+    "susy/output/shared/_margin.scss",
+    "susy/output/shared/_output.scss",
+    // - Support
+    "susy/output/_support.scss"
+    "susy/output/support/_background.scss",
+    "susy/output/support/_box-sizing.scss",
+    "susy/output/support/_clearfix.scss",
+    "susy/output/support/_prefix.scss",
+    "susy/output/support/_rem.scss",
+    "susy/output/support/_support.scss"
     ], 'server', {
     isAsset: true
   });
