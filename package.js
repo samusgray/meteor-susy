9c4c86bbd3e3ac23824ce7b2a409f641138f2aae
= <jason@tiller.io>
2015-09-02 13:07:01 -0700

    Update fourseven:scss dependency

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'meta:susy',
   summary: 'Powerful grids for the web.',
-  version: '2.1.1',
+  version: '2.2.0',
   git: 'https://github.com/aaronagray/meteor-susy'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('fourseven:scss@2.0.0-beta');
+  api.use('fourseven:scss@3.2.0');
   api.addFiles([
 
     // Susy API
@@ -61,9 +61,8 @@ Package.onUse(function(api) {
     "susy/output/support/_prefix.scss",
     "susy/output/support/_rem.scss",
     "susy/output/support/_support.scss"
-    ], 'server', {
-    isAsset: true
-  });
+    ], 'server', {isAsset: true});
+  api.export('Susy');
 });
 
 Package.onTest(function(api) {
